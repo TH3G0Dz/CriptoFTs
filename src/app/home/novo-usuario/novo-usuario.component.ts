f3e59c49ebf013b3743fc82964d5c30092b70eb3
Luiz Eduardo Domingues <domingues-pc@hotmail.com>
2022-11-14 18:01:59 -0400

    CryptoFt login cadastro

---
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { minusculoValidator } from './minusculo.validator';
+import { NovoUsuario } from './novo-usuario';
+import { NovoUsuarioService } from './novo-usuario.service';
+import { UsuarioExisteService } from './usuario-existe.service';
+import { usuarioSenhaIguaisValidator } from './usuario-senha-iguais.validator';
+
+@Component({
+  selector: 'app-novo-usuario',
+  templateUrl: './novo-usuario.component.html',
+  styleUrls: ['./novo-usuario.component.css'],
+})
+export class NovoUsuarioComponent implements OnInit {
+  novoUsuarioForm!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private novoUsuarioService: NovoUsuarioService,
+    private usuarioExistenteServive: UsuarioExisteService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.novoUsuarioForm = this.formBuilder.group(
+      {
+        userName: [
+          '',
+          [minusculoValidator],
+          [this.usuarioExistenteServive.usuarioJaExite()],
+        ],
+        email: ['', [Validators.required, Validators.email]],
+        password: [''],
+        repassword: ['']
+      },
+      {
+        validators: [usuarioSenhaIguaisValidator],
+      }
+    );
+  }
+
+  cadastrar() {
+    if (this.novoUsuarioForm.valid) {
+      const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
+      this.novoUsuarioService.cadastraNovoUsuario(novoUsuario).subscribe(
+        () => {
+          this.router.navigate(['']);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    }
+  }
+}
